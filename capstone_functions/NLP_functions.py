c6cd63c5e0959198b44fb00f3bf5d336911aaff8
anna-dang <annaadangela@gmail.com>
2021-01-17 01:32:46 -0500

    evaluation function

--- capstone_functions/NLP_functions.py
+++ capstone_functions/NLP_functions.py
@@ -1,9 +1,12 @@
 """ This file contains functions to for NLP and modeling for my capstone project. 01/14/2021 """
 
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import auc, accuracy_score, confusion_matrix, classification_report, roc_auc_score
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+from wordcloud import WordCloud
 
 # # from medium
 # def preprocess_text(text):
@@ -36,21 +39,61 @@ def process_review(review):
 #processed_data = list(map(process_review, data))
 
 
-def plot_cloud(df, rating=int, pattern=None, stopwords=None):
+
+def make_cloud(df, rating=int, pattern=None, stopwords=None):
     
-    reviews = df.loc[df['Rating'] == rating]['Review']
+    if 1 <= rating <= 5:
+        
+        reviews = df.loc[df['Rating'] == rating]['Review']
+        text = " ".join(review for review in reviews)
+
+    else:
+        
+        reviews = df['Review']
+        text = " ".join(review for review in reviews)
     
-    text = " ".join(review for review in reviews)
-    
-    wordcloud = WordCloud(random_state=seed, colormap='plasma', 
-                          collocations=False, regexp=pattern, 
+    wordcloud = WordCloud(random_state=619, colormap='plasma', 
+                          collocations=False, regexp=pattern,
+                          width=400, height=300,
+                          font_path="./driver/tommy_font.otf",
                           background_color="white",
                           stopwords=stopwords).generate(text)
     
-    plt.figure(figsize=(40, 30))
-    plt.imshow(wordcloud, interpolation='bilinear') 
+    return wordcloud
+
+
+
+def plot_cloud(wordcloud):
+    plt.figure(figsize=(15,10))
+    plt.imshow(wordcloud, interpolation='bilinear', aspect='auto') 
     plt.axis("off");
 
+
+
+def plot_clouds_per_rating(df, pattern=None, stopwords=None):
+
+    fig, axes = plt.subplots(3, 2, figsize=(25, 30))
+    fig.tight_layout(pad=0.2, w_pad=0.5, h_pad=1.2)
+
+    for i, ax in zip(list(range(6, 0, -1)), axes.flatten()):
+
+        if 1 <= i-1 <= 5:
+            ax.imshow(make_cloud(df, rating=(i-1), pattern=pattern, stopwords=stopwords), 
+                                interpolation="bilinear")
+            ax.axis("off")
+            ax.set_title(f"- - - {i-1} Stars - - -", size=25, fontweight='bold')
+
+        else:
+            ax.axis('off')
+
+
+
+def multiclass_roc_auc_score(y_test, y_pred, average="macro"):
+    lb = LabelBinarizer()
+    lb.fit(y_test)
+    y_test = lb.transform(y_test)
+    y_pred = lb.transform(y_pred)
+    return roc_auc_score(y_test, y_pred, average=average)
 
 
 
@@ -63,52 +106,39 @@ def evaluate_model(model, X, labels, return_preds=False, norm_type='true'):
         Optional: return predicted labels."""
 
     # Get binary predictions by round predicted probabilities
-    y_hat = np.concatenate(model.predict(X).round())
+    y_hat = model.predict(X)
    
     # Print classification report
-    print("---"*20)
-    print("Classification Report for Test Data: \n")
-    print(metrics.classification_report(labels, y_hat))
-    print("---"*20)
-
-    # Print model performance
-    print("Loss of the model is - " , model.evaluate(X, labels)[0])
-    print("Accuracy of the model is - " , model.evaluate(X, labels)[1]*100 , "%")
-    print("---"*20)
+    print("Classification Report: \n")
+    print(classification_report(labels, y_hat))
 
     # Print ratio of predictions
-    correct = np.sum(y_hat == test_labels)
-    incorrect = np.sum(y_hat != test_labels)
-    print(f"Correct: {correct}, {round((correct/len(test_labels)*100), 2)}%") 
-    print(f"Incorrect: {incorrect}, {round((incorrect/len(test_labels)*100), 2)}%")
-    print("---"*20)
+    correct = np.sum(y_hat == labels)
+    incorrect = np.sum(y_hat != labels)
+    print(f"Correct: {correct}, {round((correct/len(labels)*100), 2)}%") 
+    print(f"Incorrect: {incorrect}, {round((incorrect/len(labels)*100), 2)}%")
+    
+    # ROC AUC score
+    roc_auc = multiclass_roc_auc_score(labels, y_hat, average='macro')
+    print(f"ROC/AUC: {round(roc_auc, 3)}")
 
     # Build confusion matrix
-    print("Model Prediction Results for Test Data:")
-    cm = metrics.confusion_matrix(labels, y_hat, normalize='all')
-    cm_true = metrics.confusion_matrix(labels, y_hat, normalize='true')
+    cm = confusion_matrix(labels, y_hat, normalize='all')
+    cm_true = confusion_matrix(labels, y_hat, normalize='true')
 
     # Set figure
-    fig, (ax1, ax2, ax3) = plt.subplots(1,3,figsize=(16,4) )
+    fig, (ax1, ax2) = plt.subplots(1,2,figsize=(25, 10))
 
     # Plot quantity distribution confusion matrix
-    sns.heatmap(cm, annot=True, fmt='.0%', cmap="BuPu", square=True, ax=ax1);
+    sns.heatmap(cm, annot=True, fmt='.0%', cmap="BuPu", ax=ax1);
     ax1.set(title='Distribution of Predictions',ylabel='True Class', xlabel='Predicted Class')
+    ax1.set_ylim(5.0, 0)
 
     # Plot normalized matrix (to ROWS, true values)
-    sns.heatmap(cm_true, annot=True, fmt='.0%', cmap="BuPu", square=True, ax=ax2);
+    sns.heatmap(cm_true, annot=True, fmt='.0%', cmap="BuPu", ax=ax2);
     ax2.set(title='Normalized to True Class',ylabel='True Class', xlabel='Predicted Class')
-
-    # Calculate 'false-positive rate', 'true-positive rate' and 'area under curve' (AUC)
-    fpr , tpr , thresholds = metrics.roc_curve(labels, y_hat)
-    auc_score = metrics.roc_auc_score(labels, y_hat)
-
-    ax3.plot(fpr,tpr, color='indigo'); 
-    ax3.axis([0,1,0,1]) 
-    ax3.set(xlabel ='False Positive Rate', ylabel ='True Positive Rate', 
-            title = f"ROC Curve, AUC = {auc_score:.2f}")
-    #ax3.title(f"ROC Curve, AUC: {round(auc_score), 3}", size=15)
-    ax3.grid(color='whitesmoke', zorder=0)
+    ax2.set_ylim(5.0, 0)
+    
     plt.show()
 
     # If selected, return predicted labels
